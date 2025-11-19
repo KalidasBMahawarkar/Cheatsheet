310c05010a8725266e1f359d330c9dc2f17724ac
KalidasBMahawarkar <93465818+KalidasBMahawarkar@users.noreply.github.com>
2025-11-19 12:49:58 +0530

    video

---
+++ test.js
@@ -0,0 +1,42 @@
+if(){
+    goto step3;
+
+}
+
+var j = 10; 
+var j = fun1(); 
+var adi = "// name"
+step1 : for (let i = 0 ; i< 10; i++){
+     i++;
+     
+    //logic
+    return ;
+    (a+b
+         *dhgvkuih )
+    j
+}
+
+
+
+step2 : for (let i = 0 ;
+     i< 10; i++){
+    //logic
+}
+
+
+
+step3 : for (let i = 0 ; i< 10; i++){
+    //logic
+}
+
+while(true);
+
+{
+    //logic
+}
+
+//# 
+
+let jsonn = {
+    // a: b
+}
